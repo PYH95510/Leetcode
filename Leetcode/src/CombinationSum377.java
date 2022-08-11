01608e38ac41155b83614c81797c66fa4c79c62c
Yohan Park <pyh95510@gmail.com>
2022-08-10 22:57:10 -0400

    How to understand the codes

--- Leetcode/src/CombinationSum377.java
+++ Leetcode/src/CombinationSum377.java
@@ -14,7 +14,16 @@ public class CombinationSum377 {
 	        //Better to think it as math problem when we solve dynamic programming
 	        for(int i=1;i<dp.length;i++){ 
 	           for (int j=0 ; j<nums.length ; j ++){
-	                if (nums[j] <= i) dp[i] += dp[i-nums[j]];
+	                if (nums[j] <= i) dp[i] += dp[i-nums[j]]; // This is very similar to coin change problem
+	                
+	                // when you write if statement, just write it accordingly to dp[i - nums[j]]. try to 
+	                // avoid out of index boundary error
+	                
+	                //However, we don't need to find minimum value. We need to find different combination
+	                //Therefore, if you find any, add that amount to dp[i] and then find the rest's combination
+	                //Forexample if we want to find target 7, dp[i] is 7 and num[i] is 4 then we need to find 3 so we should
+	                //Find 7-4 so it should be 4-3 and then find the rest.
+	                
 	        }
 	        
 	        
