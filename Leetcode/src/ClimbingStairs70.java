c7f05febc134a63d7fa92cbb6856862f57d26a8b
Yohan Park <Yohan Park@DESKTOP-31PQH2A>
2022-08-12 22:55:53 -0400

    Climbing Stairs DP solution and optimized solution with comments

--- Leetcode/src/ClimbingStairs70.java
+++ Leetcode/src/ClimbingStairs70.java
@@ -40,7 +40,7 @@ public class ClimbingStairs70 {
         
         int[] dp = new int[n + 1];
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2] =  2;
 
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
