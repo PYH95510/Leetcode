b971228a3cb7fa7443337f6c5f75d30e808c4c03
Yohan Park <pyh95510@gmail.com>
2022-08-22 18:41:13 -0400

    change the whole method except the concept of dp

--- src/practicing/UglyNumber2_264.java
+++ src/practicing/UglyNumber2_264.java
@@ -8,27 +8,24 @@ public class UglyNumber2_264 {
 	    public int nthUglyNumber(int n) {
 	        if(n==1) return 1;
 	        
-	        int [] dp = new int[n+1];
-	        int [] prime = new int[3];
-	        prime[0] = 2;
-	        prime[1] = 3;
-	        prime[2] = 5;
+	        int [] dp = new int[n];
+	        
+	        
 	        //assigning prime each prime numbers to prime array
-
-	        Arrays.fill(dp, Integer.MAX_VALUE);
+	        int p2=1, p3=1, p5=1; //assigning p2,p3,p5 for each prime index
+	        
+	        
+	        
 	        
 	        dp[0] = 0;
 	        dp[1] = 1;
 	        
+	        
 	        for(int i = 2; i <dp.length ; i++){
-	            for(int j = 0 ; j < prime.length; j++){
-	                if(i>prime[j]){
-	                    
-	                    dp[i] = Math.min(dp[i],dp[i-1]+prime[j]);
-	                    
-	                    
-	                }//dp starts if statement
-	            }//second for loop
+	            int next2 = dp[p2] * 2;
+	            int next3 = dp[p3] * 3;
+	            int next5 = dp[p5] * 5; //allocating each index in other words, computing each index for each 2,3,5
+	            
 	            
 	            
 	        }//first for loop
