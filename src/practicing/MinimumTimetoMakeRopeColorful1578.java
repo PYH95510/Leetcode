ebd5305f1eefa302e50757a365bc9affd861a144
Yohan Park <pyh95510@gmail.com>
2022-08-27 23:34:24 -0400

    solution and comment

--- src/practicing/MinimumTimetoMakeRopeColorful1578.java
+++ src/practicing/MinimumTimetoMakeRopeColorful1578.java
@@ -17,3 +17,10 @@ public class MinimumTimetoMakeRopeColorful1578 {
 	}
 
 }
+/*It was really hard for me to come up with my own solution for this problem so I would like to share soluton from leet code discussion session
+ * https://leetcode.com/problems/minimum-time-to-make-rope-colorful/discuss/841690/My-Java-Solution-O(n) it is originated from here. The author first
+ * Instantiate needed variables. and then update result when we face same character from the string s. After that, the author add minimum value to the
+ * result variable and then update the cost with the maximum value because we no longer have the minimum cost because we took it out from the cost
+ * array. I was thinking about same solution but because I misunderstood the question, I failed to come up with the solution so there is possiblity
+ * for people who loop up my diary so I would like to someone else's solution! */
+
