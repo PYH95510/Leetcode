3fe7dadd301a38e21127130af340421d620a7ca0
Yohan Park <pyh95510@gmail.com>
2022-08-28 23:34:09 -0400

    push all

--- src/practicing/CoinChange322.java
+++ src/practicing/CoinChange322.java
@@ -23,3 +23,10 @@ public class CoinChange322 {
 	}
 
 }
+/*This is the most basic type of dynamic programming problem. It is really great to understand basic algorithm and method how to solve dynamic
+ * programming so I highly recommend to those who just start studying dynamic programming, take a lot of time and put as much effort as you can
+ * to this problem and try to understand top to bottom. It is okay that to understand and solve whole day. It is that much important. Use every
+ * method and means to digest. To understand this problem, there are a lot of keys we should know. First, you should fill the array with large
+ * number. Here I just arbitrarily fill with dp.length but it could be Integer.MAX_VALUE or whatever you think it is big enough. After that we should
+ * initialize array. Then we are going to iterate two for loops */
+ 
