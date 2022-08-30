c113cefc374825cb3458a67d90254b8a994c8322
Yohan Park <pyh95510@gmail.com>
2022-08-30 16:19:59 -0400

    basic idea how to solve

--- src/practicing/BurstBalloons312.java
+++ src/practicing/BurstBalloons312.java
@@ -2,8 +2,19 @@ package practicing;
 
 public class BurstBalloons312 {
 
-	public BurstBalloons312() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public int maxCoins(int[] nums) {
+	         int n = nums.length;
+	        int arr[] = new int[n+2];
+	        arr[0] = arr[n+1] = 1;   // Giving padding of 1 to the corner elements
+	        for(int i=1;i<=n;i++){
+	            arr[i] = nums[i-1];   //final padded array
+	        }
+	       
+	        
+	    }
+	    
+	    
 	}
 
 }
