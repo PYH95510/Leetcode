bac0ce3fb224cb1259b399c7e418bf48173b0d1b
Yohan Park <pyh95510@gmail.com>
2022-08-31 23:38:38 -0400

    solution for winner 486

--- src/practicing/PredicttheWinner486.java
+++ src/practicing/PredicttheWinner486.java
@@ -2,8 +2,20 @@ package practicing;
 
 public class PredicttheWinner486 {
 
-	public PredicttheWinner486() {
-		// TODO Auto-generated constructor stub
+	public class Solution {
+	    public boolean PredictTheWinner(int[] nums) {
+	          int[][] dp = new int[nums.length][nums.length];
+	        for (int i = 0; i < nums.length; i++) {
+	            dp[i][i] = nums[i];
+	        }
+	        
+	        for (int i = nums.length - 2; i >= 0; i--) {
+	            for (int j = i + 1; j < nums.length; j++) {
+	                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+	            }//checking numbers from the end, adding which number is bigger either adding from end or adding from 
+	        }        
+	        return dp[0][nums.length - 1] >= 0;
+	    }
 	}
 
 }
