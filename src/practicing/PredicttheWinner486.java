a0958d66b251052f7c9aa935a6382f0d22342e00
Yohan Park <pyh95510@gmail.com>
2022-09-01 21:25:59 -0400

    another way to do 2d dp

--- src/practicing/PredicttheWinner486.java
+++ src/practicing/PredicttheWinner486.java
@@ -47,7 +47,20 @@ public class PredicttheWinner486 {
 	}
 
 
-	
+	public class Solution {
+	    public boolean PredictTheWinner(int[] nums) {
+	        int[][] dp = new int[nums.length][nums.length];
+	        for (int s = nums.length-1; s >= 0; s--) {       
+	            dp[s][s] = nums[s]; /*+++++++++++++++++ add a new line ++++++++++++++++++*/
+	            for (int e = s+1; e < nums.length; e++) {
+	                int a = nums[s] - dp[s + 1][e];
+	                int b = nums[e] - dp[s][e - 1];
+	                dp[s][e] = Math.max(a, b);
+	            }
+	        }
+	        return dp[0][nums.length - 1] >= 0;
+	    }
+	}
 
 }
 
