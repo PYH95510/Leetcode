c6bf13c70897523beea70b81278a0acb489d31cb
Yohan Park <pyh95510@gmail.com>
2022-09-04 23:16:37 -0400

    solution and comment

--- src/practicing/GreatestSumDivisiblebyThree1262.java
+++ src/practicing/GreatestSumDivisiblebyThree1262.java
@@ -2,8 +2,45 @@ package practicing;
 
 public class GreatestSumDivisiblebyThree1262 {
 
-	public GreatestSumDivisiblebyThree1262() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public int maxSumDivThree(int[] nums) {
+	        int sum = 0;
+	        int oneRemainder = 20000;
+	        int twoRemainder = 20000;
+	        
+	        for(int num : nums)
+	        {
+	            sum += num;
+	            
+	            if(num % 3 == 1)
+	            {
+	                twoRemainder = Math.min(twoRemainder, oneRemainder + num);
+	                oneRemainder = Math.min(oneRemainder, num); 
+	            }
+	            
+	            if(num % 3 == 2)
+	            {
+	                oneRemainder = Math.min(oneRemainder, twoRemainder + num);
+	                twoRemainder = Math.min(twoRemainder, num);
+	                 
+	            }
+	        }
+	        
+	        if(sum % 3 == 0)
+	            return sum;
+	        
+	        if(sum % 3 == 1)
+	            return sum - oneRemainder;
+	        
+	        if(sum % 3 == 2)
+	            return sum - twoRemainder;
+	        
+	        return 0;
+	            
+	    }
 	}
 
 }
+/*While I was trying to solve this problem, I found out that this problem was kind of waste of time. This problem could be easily solved. We do not actually
+ * need serious algorithmic thoughts or coding skills. All we need is just divide every numbers into case by case situations and then subtract them 
+ * from the total sum from minimum number. That was it. */
