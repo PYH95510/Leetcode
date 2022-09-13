a91cc8765aed50660c6d7c5fc8fd22555e89e6c4
Yohan Park <pyh95510@gmail.com>
2022-09-12 23:08:38 -0400

    flip string

---
+++ src/practicing/FlipStringtoMonotoneIncreasing926.java
@@ -0,0 +1,21 @@
+package practicing;
+
+public class FlipStringtoMonotoneIncreasing926 {
+
+	class Solution {
+	    public int minFlipsMonoIncr(String s) {
+	        int zeros = 0;
+	        int ones = 0;
+	        
+	        
+	        for(int i = 0 ; i<s.length(); i++){
+	            
+	            
+	        }
+	        
+	        return ones;
+	        
+	    }
+	}
+
+}
