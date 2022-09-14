112e6a2ec24102d17f6cf5ed3c7dabe52f072482
Yohan Park <pyh95510@gmail.com>
2022-09-13 20:55:33 -0400

    dp solution

--- src/practicing/IntegerBreak343.java
+++ src/practicing/IntegerBreak343.java
@@ -2,8 +2,29 @@ package practicing;
 
 public class IntegerBreak343 {
 
-	class solution{
-		
+	class Solution {
+	    public int integerBreak(int n) {
+	        // first divide the integer n into several pieces that becomes 10 after being added
+	        
+	        /*dp[n+1]
+	        dp[i]
+	        */
+	        int dp[] = new int [n+1];
+	        dp[0] = 1;
+	        
+	        
+	        
+	        for(int i = 2 ; i < dp.length ; i++){ //because n is greater or eqaul than 2
+	            for(int j = 1 ; j < i ; j++){
+	                dp[i] = Math.max(dp[i], (Math.max(j,dp[j]) * Math.max(i-j,dp[i-j])));
+	            }
+	        }
+	        
+	        return dp[n];
+	        
+	    }
+	    
+	    
 	}
 	
 
