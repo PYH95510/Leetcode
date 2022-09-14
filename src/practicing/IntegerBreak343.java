46fdf77dc90e151f6391f54782f72765b989a89f
Yohan Park <pyh95510@gmail.com>
2022-09-13 20:59:34 -0400

    math solution added

--- src/practicing/IntegerBreak343.java
+++ src/practicing/IntegerBreak343.java
@@ -28,5 +28,20 @@ public class IntegerBreak343 {
 	    
 	}
 	
+//	public class Solution {
+//	    public int integerBreak(int n) {
+//	        if(n == 2)
+//	            return 1;
+//	        else if(n == 3)
+//	            return 2;
+//	        else if(n%3 == 0)
+//	            return (int)Math.pow(3, n/3);
+//	        else if(n%3 == 1)
+//	            return 2 * 2 * (int) Math.pow(3, (n - 4) / 3);
+//	        else 
+//	            return 2 * (int) Math.pow(3, n/3);
+//	    }
+//	            
+//	} MATH SOLUTION
 
 }
