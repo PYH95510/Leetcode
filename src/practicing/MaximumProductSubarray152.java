5679e653025b59272e8a338b11d911ec1756ddc8
Yohan Park <pyh95510@gmail.com>
2022-09-16 23:42:07 -0400

    error fixed

--- src/practicing/MaximumProductSubarray152.java
+++ src/practicing/MaximumProductSubarray152.java
@@ -2,8 +2,30 @@ package practicing;
 
 public class MaximumProductSubarray152 {
 
-	public MaximumProductSubarray152() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public int maxProduct(int[] nums) {
+	        
+	        int max = nums[0] , min = nums[0] , ans = nums[0] ;
+	        
+	        for(int i = 1 ; i < nums.length ; i++){
+	            
+	            int tmt = max ;
+	            max = Math.max(Math.max(max*nums[i], min*nums[i]), nums[i]);
+	            min = Math.min(Math.min(tmt*nums[i], min*nums[i]), nums[i]);
+	            
+	            if(max>ans)ans=max;
+	            
+	            
+	            
+	        }
+	        
+	        
+	        return ans;
+
+	    }
+	    
+	    
 	}
 
 }
+/**/
