df59361a8c8f0489224b64f3b5ff86a6bd305a56
Yohan Park <pyh95510@gmail.com>
2022-09-17 00:51:01 -0400

    Edit Distance, hard

---
+++ src/practicing/EditDistance72.java
@@ -0,0 +1,12 @@
+package practicing;
+
+public class EditDistance72 {
+
+	public EditDistance72() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+/*Levenshtein distance, Wagner–Fischer is what we should know from this problem. This problem is pretty much same as Levenshtein distance and
+ * it could be easily solve if we are using Wagner-Fischer algorithm. */
