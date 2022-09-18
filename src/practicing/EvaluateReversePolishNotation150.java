bf2691d13c062e56873304ede6de5a98425696f9
Yohan Park <pyh95510@gmail.com>
2022-09-18 01:09:54 -0400

    minor change

--- src/practicing/EvaluateReversePolishNotation150.java
+++ src/practicing/EvaluateReversePolishNotation150.java
@@ -10,23 +10,23 @@ public class EvaluateReversePolishNotation150 {
 			Stack<Integer> stack = new Stack<Integer>();
 			for (String s : tokens) {
 				if(s.equals("+")) {
-					stack.add(stack.pop()+stack.pop());
+					stack.push(stack.pop()+stack.pop());
 				}
 				else if(s.equals("/")) {
 					b = stack.pop();
 					a = stack.pop();
-					stack.add(a / b);
+					stack.push(a / b);
 				}
 				else if(s.equals("*")) {
-					stack.add(stack.pop() * stack.pop());
+					stack.push(stack.pop() * stack.pop());
 				}
 				else if(s.equals("-")) {
 					b = stack.pop();
 					a = stack.pop();
-					stack.add(a - b);
+					stack.push(a - b);
 				}
 				else {
-					stack.add(Integer.parseInt(s));
+					stack.push(Integer.parseInt(s));
 				}
 			}	
 			return stack.peek();
