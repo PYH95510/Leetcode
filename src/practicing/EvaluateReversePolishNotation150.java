00e3dbb29fd3dd3189344ed670b4696d6f26af8e
Yohan Park <pyh95510@gmail.com>
2022-09-18 01:26:28 -0400

    solution and comment

--- src/practicing/EvaluateReversePolishNotation150.java
+++ src/practicing/EvaluateReversePolishNotation150.java
@@ -34,4 +34,8 @@ public class EvaluateReversePolishNotation150 {
 	}
 
 }
-/*Fortunately, I could easily solve this problem because I have done a project that was very much similar to this*/
+/*Fortunately, I could easily solve this problem because I have done a project that was very much similar to this. If you can understand what stack
+ * does, it is really easy to solve this. When you encounters numbers, you just push it to the stack and if you encounters operands, you pop two
+ * numbers and then do the operations. From the problem, it says you should do the round down to 0 when you do the / operation
+ *  but it is unnecessary in JAVA because JAVA does it automatically. THe basic time complexity of this algorithm is O(n) and the space complexity is
+ *  O(n) as well because we are using stack memory.*/
