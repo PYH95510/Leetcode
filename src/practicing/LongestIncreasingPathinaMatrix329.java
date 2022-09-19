52f84a43bd6ffd5e9b37b9aabad2daad2a9baccf
Yohan Park <pyh95510@gmail.com>
2022-09-19 15:49:28 -0400

    dp memoized solution

--- src/practicing/LongestIncreasingPathinaMatrix329.java
+++ src/practicing/LongestIncreasingPathinaMatrix329.java
@@ -2,6 +2,38 @@ package practicing;
 
 public class LongestIncreasingPathinaMatrix329 {
 
+//	class Solution {
+//	    private static final int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+//	    private int m,n;
+//	    
+//	    public int longestIncreasingPath(int[][] matrix) {
+//	        if(matrix.length == 0) return 0;
+//	        m = matrix.length;
+//	        n = matrix[0].length;
+//	        int ans = 0;
+//	        
+//	        
+//	        for(int i = 0 ; i<m; i++)
+//	            for(int j = 0; j<n; j++)
+//	                ans = Math.max(ans,dfs(matrix,i,j));
+//	        return ans;
+//	    }
+//	    
+//	    public int dfs(int[][] matrix , int i, int j){
+//	        int ans = 0;
+//	        for(int[]d : directions){
+//	            int x = i+d[0] , y = j + d[1];
+//	            if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
+//	                ans = Math.max(ans,dfs(matrix,x,y));
+//	        }
+//	        
+//	            return ++ans;
+//	        
+//	    }
+//	    
+//	    
+//	    
+//	}
 	class Solution {
 	    private static final int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 	    private int m,n;
@@ -10,24 +42,26 @@ public class LongestIncreasingPathinaMatrix329 {
 	        if(matrix.length == 0) return 0;
 	        m = matrix.length;
 	        n = matrix[0].length;
+	        int[][] cache = new int[m][n];
 	        int ans = 0;
 	        
 	        
 	        for(int i = 0 ; i<m; i++)
 	            for(int j = 0; j<n; j++)
-	                ans = Math.max(ans,dfs(matrix,i,j));
+	                ans = Math.max(ans,dfs(matrix,i,j,cache));
 	        return ans;
 	    }
 	    
-	    public int dfs(int[][] matrix , int i, int j){
-	        int ans = 0;
+	    public int dfs(int[][] matrix , int i, int j, int[][] cache){
+	        if(cache[i][j] !=0 ) return cache[i][j]; // if it is already computed, we check it so we do not need future computation.
+	        
 	        for(int[]d : directions){
 	            int x = i+d[0] , y = j + d[1];
 	            if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
-	                ans = Math.max(ans,dfs(matrix,x,y));
+	                cache[i][j] = Math.max(ans,dfs(matrix,x,y,cache)); //if it is computed, assign the values to cache[i][j]
 	        }
 	        
-	            return ++ans;
+	            return ++cache[i][j];
 	        
 	    }
 	    
