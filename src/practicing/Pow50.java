7c49bc6b9aedf37c8855ec0bed348a40d93c1e75
Yohan Park <pyh95510@gmail.com>
2022-09-21 15:42:15 -0400

    brute force solution

---
+++ src/practicing/Pow50.java
@@ -0,0 +1,36 @@
+package practicing;
+
+public class Pow50 {
+
+//	class Solution {
+//		public double myPow(double x, int n) {
+//			
+//			if(n == 0 )return 1;
+//			
+//			long N = n;
+//			
+//			
+//			if(N <0) {
+//				x = 1/x;
+//				N = -N;
+//			}
+//			
+//			double ans = 1;
+//			
+//			for(long i = 0; i < N ; i++) {
+//				ans = ans * x;
+//			}
+//				
+//				
+//			
+//			
+//			
+//			return ans;
+//			
+//		}
+//	}
+	
+	
+
+}
+
