ad4979bfa200490772207f26313b8d3a46c3f95b
Yohan Park <pyh95510@gmail.com>
2022-09-22 22:41:10 -0400

    merge intervals basic idea

---
+++ src/practicing/MergeIntervals56.java
@@ -0,0 +1,23 @@
+package practicing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MergeIntervals56 {
+
+	public class Solution{
+		 public int[][] merge(int[][] intervals) {
+			 Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+			 LinkedList<int[]> merge = new LinkedList<>();
+			 
+			 for(int[] interval : intervals) { 
+				 
+			 }
+			 
+		 }
+		 
+		 
+		 
+	}
+
+}
