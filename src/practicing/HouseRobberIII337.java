c2517dd12c9e877150bae248edaaf6e28207091c
Yohan Park <pyh95510@gmail.com>
2022-09-27 01:01:00 -0400

    re writing

--- src/practicing/HouseRobberIII337.java
+++ src/practicing/HouseRobberIII337.java
@@ -19,24 +19,28 @@ public class HouseRobberIII337 {
 	 */
 	class Solution {
 	    public int[] helper(TreeNode node) {
-	        // return [rob this node, not rob this node]
-	        if (node == null) {
-	            return new int[] { 0, 0 };
-	        }
-	        int left[] = helper(node.left);
-	        int right[] = helper(node.right);
-	        // if we rob this node, we cannot rob its children
-	        int rob = node.val + left[1] + right[1];
-	        // else, we free to choose rob its children or not
-	        int notRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-
-	        return new int[] { rob, notRob };
+	       if(node == null)return new int [] {0,0}; //reaching to base case.
+	    
+	       int left[] = helper(node.left);
+	       int right[] = helper(node.right);
+	       
+	       int rob = node.val + left[1] + right[1]; //we will setting array[1] to not robbed values
+	       int notrobbed = Math.max(left[0],left[1])+Math.max(right[0], right[1]);
+	       
+	       return new int [] {rob, notrobbed};
+	       
+	       
 	    }
-
+	    
+	    
 	    public int rob(TreeNode root) {
-	        int[] answer = helper(root);
-	        return Math.max(answer[0], answer[1]);
+	    	int [] answer = helper(root);
+	    	return Math.max(answer[0], answer[1]);
 	    }
+
+	   
 	}
 
 }
+
+//[3,2,3,2,3,null,1]
