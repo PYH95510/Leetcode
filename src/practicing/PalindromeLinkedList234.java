8b264e8a0c32f3a841c249cd210591b52102941d
Yohan Park <pyh95510@gmail.com>
2022-09-28 01:54:13 -0400

    time o(n) space o(n) arraylist solution

---
+++ src/practicing/PalindromeLinkedList234.java
@@ -0,0 +1,47 @@
+package practicing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList234 {
+
+	
+	  //Definition for singly-linked list.
+	  public class ListNode {
+	      int val;
+	      ListNode next;
+	     ListNode() {}
+	     ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+	
+	class Solution {
+	    public boolean isPalindrome(ListNode head) {
+	        
+	        List <Integer> vals = new ArrayList <Integer>();
+	        
+	        ListNode currentNode = head;
+	        
+	        while(currentNode != null){
+	            vals.add(currentNode.val);
+	            currentNode = currentNode.next;
+	        }
+	        
+	        int front = 0;
+	        int back = vals.size()-1;
+	        
+	        while (front < back){
+	            if (!vals.get(front).equals(vals.get(back))){
+	                return false;
+	            }
+	            front ++;
+	            back --;
+	        }
+	        
+	        return true;
+	    }
+	    
+	    
+	}
+
+}
