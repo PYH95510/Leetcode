7c2d6026aa250b895d90e6bd5c36d558a4ec5fc0
Yohan Park <pyh95510@gmail.com>
2022-09-29 20:43:12 -0400

    recursive solution with time o(n) space o(n)

--- src/practicing/PalindromeLinkedList234.java
+++ src/practicing/PalindromeLinkedList234.java
@@ -15,33 +15,52 @@ public class PalindromeLinkedList234 {
 	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	  }
 	
-	class Solution {
-	    public boolean isPalindrome(ListNode head) {
-	        
-	        List <Integer> vals = new ArrayList <Integer>();
-	        
-	        ListNode currentNode = head;
-	        
-	        while(currentNode != null){
-	            vals.add(currentNode.val);
-	            currentNode = currentNode.next;
-	        }
-	        
-	        int front = 0;
-	        int back = vals.size()-1;
-	        
-	        while (front < back){
-	            if (!vals.get(front).equals(vals.get(back))){
-	                return false;
-	            }
-	            front ++;
-	            back --;
-	        }
-	        
-	        return true;
-	    }
-	    
-	    
-	}
+//	class Solution {
+//	    public boolean isPalindrome(ListNode head) {
+//	        
+//	        List <Integer> vals = new ArrayList <Integer>();
+//	        
+//	        ListNode currentNode = head;
+//	        
+//	        while(currentNode != null){
+//	            vals.add(currentNode.val);
+//	            currentNode = currentNode.next;
+//	        }
+//	        
+//	        int front = 0;
+//	        int back = vals.size()-1;
+//	        
+//	        while (front < back){
+//	            if (!vals.get(front).equals(vals.get(back))){
+//	                return false;
+//	            }
+//	            front ++;
+//	            back --;
+//	        }
+//	        
+//	        return true;
+//	    }
+//	    
+//	    
+//	}
+	  
+	  class Solution {
+
+		    private ListNode frontPointer;
+
+		    private boolean recursivelyCheck(ListNode currentNode) {
+		        if (currentNode != null) {
+		            if (!recursivelyCheck(currentNode.next)) return false;
+		            if (currentNode.val != frontPointer.val) return false;
+		            frontPointer = frontPointer.next;
+		        }
+		        return true;
+		    }
+
+		    public boolean isPalindrome(ListNode head) {
+		        frontPointer = head;
+		        return recursivelyCheck(head);
+		    }
+		}
 
 }
