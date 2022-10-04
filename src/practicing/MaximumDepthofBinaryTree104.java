31cabd0a23224e54cf35502eb6a0b37b1a9a71bf
Yohan Park <pyh95510@gmail.com>
2022-10-03 22:45:28 -0400

    tree node class, and answer

--- src/practicing/MaximumDepthofBinaryTree104.java
+++ src/practicing/MaximumDepthofBinaryTree104.java
@@ -2,8 +2,34 @@ package practicing;
 
 public class MaximumDepthofBinaryTree104 {
 
-	public MaximumDepthofBinaryTree104() {
-		// TODO Auto-generated constructor stub
+	
+	
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	     }
+	  }
+	 
+	
+	class Solution {
+	    public int maxDepth(TreeNode root) {
+	        int max = 0;
+	        
+	        if(root == null) return max;
+	        
+	        
+	        int right = maxDepth(root.right);
+	        int left = maxDepth(root.left);
+	        
+	        return Math.max(left, right)+1;
+	    }
 	}
 
 }
