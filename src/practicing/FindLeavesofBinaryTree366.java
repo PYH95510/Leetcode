23943fede18cfbb61f842d133ab8a2bdd8d4a0b6
Yohan Park <pyh95510@gmail.com>
2022-10-05 01:42:53 -0400

    O(N) solution not using sorting

--- src/practicing/FindLeavesofBinaryTree366.java
+++ src/practicing/FindLeavesofBinaryTree366.java
@@ -70,10 +70,43 @@ public class FindLeavesofBinaryTree366 {
 //	    
 //	    
 //	}
-	
+	  class Solution {
+		    private List<List<Integer>> solution;
+		    
+		    private int getHeight(TreeNode root){
+		        if (root == null)return -1;
+		        
+		        int leftHeight = getHeight(root.left);
+		        int rightHeight = getHeight(root.right);
+		        
+		        int currHeight = Math.max(leftHeight,rightHeight)+1;
+		        
+		        if(this.solution.size() == currHeight){
+		            this.solution.add(new ArrayList<>());
+		        }
+		        
+		        this.solution.get(currHeight).add(root.val);
+		        
+		        return currHeight;
+		        
+		    }
+		    
+		    public List<List<Integer>> findLeaves(TreeNode root) {
+		    
+		        this.solution = new ArrayList<> ();
+		        
+		        getHeight(root);
+		        
+		        return this.solution;
+		        
+		    }
+		    
+		}
 }
 /*This solution is based on sorting. The basic process of this solution is we first write method for calculating height. Using recursive method,
  * either right or left, increment one to max number. Add current height and root's number to pair. The reason that we are using pair is because
  * pair is compatible with arraylist. After we write all the cacluating height method, we then write findleves method. We first convert pairs with
  * arraylist. Then get the height. After that, sort the pairs. The reason that we are doing this is because we are going to add it iteratively.
- * After this, we are just going to add them iteratively.*/
+ * After this, we are just going to add them iteratively. However, below is so elegant and easy solution compare to the upper one. We just add
+ * the node's value to the solution array accordingly to the height. By getting this.solution.get(currHeight).add(root.val); By doing so, the
+ * solution method becomes much more easy and the solution time becomes O(n) from O(NlogN)*/
