4c91213966084135f6072793eaf98ffae8821e16
Yohan Park <pyh95510@gmail.com>
2022-10-05 01:08:56 -0400

    collection sort solution

--- src/practicing/FindLeavesofBinaryTree366.java
+++ src/practicing/FindLeavesofBinaryTree366.java
@@ -1,6 +1,7 @@
 package practicing;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FindLeavesofBinaryTree366 {
@@ -20,52 +21,59 @@ public class FindLeavesofBinaryTree366 {
 	      }
 	  }
 	 
-	class Solution {
-	    private List<Pair<Integer, Integer>> pairs; //pairs from JavaFx
-	    
-	    private int getHeight(TreeNode root) {
-	        
-	        // return -1 for null nodes
-	        if (root == null) return -1;
-	        
-	        // first calculate the height of the left and right children
-	        int leftHeight = getHeight(root.left);
-	        int rightHeight = getHeight(root.right);
-	        
-	        // based on the height of the left and right children, obtain the height of the current (parent) node
-	        int currHeight = Math.max(leftHeight, rightHeight) + 1;
-
-	        // collect the pair -> (height, val)
-	        this.pairs.add(new Pair<Integer, Integer>(currHeight, root.val));
-
-	        // return the height of the current node
-	        return currHeight;
-	    }
-	    
-	    
-	    public List<List<Integer>> findLeaves(TreeNode root) {
-	        this.pairs = new ArrayList<>();
-	        
-	        getHeight(root);
-	        
-	        // sort all the (height, val) pairs
-	        Collections.sort(this.pairs, Comparator.comparing(p -> p.getKey()));
-	        
-	        int n = this.pairs.size(), height = 0, i = 0;
-
-	        List<List<Integer>> solution = new ArrayList<>();
-	        
-	        while (i < n) {
-	            List<Integer> nums = new ArrayList<>();
-	            while (i < n && this.pairs.get(i).getKey() == height) {
-	                nums.add(this.pairs.get(i).getValue());
-	                i++;
-	            } 
-	            solution.add(nums);
-	            height++;
-	        }
-	        return solution;
-	    }
-	}
-
+//	class Solution {
+//	    private List<Pair<Integer,Integer>> piars;
+//	    
+//	    
+//	    
+//	    private int getHeight(TreeNode root) {
+//	    	if(root == null)return -1;
+//	    	
+//	    	
+//	    	
+//	    	int left = getHeight(root.left);
+//	    	int right = getHeight(root.right);
+//	    	
+//	    	
+//	    	int currHeight = Math.max(left, right)+1; 
+//	    	
+//	    	this.piars.add(new Pair<Integer,Integer>(currHeight,root.val));
+//	    	
+//	    	
+//	    	return currHeight;
+//	    }
+//	    
+//	    
+//	    private List<List<Integer>>findLeaves(TreeNode root){
+//	    	this.pairs = new ArrayList<>();
+//	    	
+//	    	getHeight(root);
+//	    	
+//	        Collections.sort(this.pairs, Comparator.comparing(p -> p.getKey()));
+//	    	ArrayList<List<Integer>>solution = new ArrayList<List<Integer>>();
+//	    	
+//	        int n = this.pairs.size() , height = 0 , i = 0;
+//	        
+//	        while(i < n) {
+//	        	List<Integer> nums = new ArrayList<Integer> ();
+//	        	while(i < n && this.pairs.get(i).getkey() == height) {
+//	        		nums.add(this.pairs.get(i).getvalue());
+//	        		i++;
+//	        	}
+//	        	solution.add(nums);
+//	        	height++;
+//	        }
+//	    	
+//	        
+//	        return solution;
+//	    }
+//	    
+//	    
+//	}
+	
 }
+/*This solution is based on sorting. The basic process of this solution is we first write method for calculating height. Using recursive method,
+ * either right or left, increment one to max number. Add current height and root's number to pair. The reason that we are using pair is because
+ * pair is compatible with arraylist. After we write all the cacluating height method, we then write findleves method. We first convert pairs with
+ * arraylist. Then get the height. After that, sort the pairs. The reason that we are doing this is because we are going to add it iteratively.
+ * After this, we are just going to add them iteratively.*/
