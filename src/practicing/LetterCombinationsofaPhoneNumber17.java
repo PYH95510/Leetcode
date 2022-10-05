cae3a72456e06fe84356d6d6f363464c388dfbd8
Yohan Park <pyh95510@gmail.com>
2022-10-05 03:02:39 -0400

    solution

--- src/practicing/LetterCombinationsofaPhoneNumber17.java
+++ src/practicing/LetterCombinationsofaPhoneNumber17.java
@@ -1,5 +1,6 @@
 package practicing;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationsofaPhoneNumber17 {
@@ -7,7 +8,29 @@ public class LetterCombinationsofaPhoneNumber17 {
 	public class solution{
 		
 		public List<String> letterCombinations(String digits){
+			List<String> answer = new ArrayList<String>();
+			String[] comp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 			
+			
+			
+			for(int i = 0 ; i<digits.length(); i++) {
+				answer = combination(comp[digits.charAt(i)-'0'],answer);
+			}
+			
+			return answer;
+		}
+		
+		
+		public List<String> combination(String comp, List<String> answer){
+			List<String> result = new ArrayList<String>();
+			
+			for(int i=0;i<comp.length();i++)
+				for(String s : answer) 
+					result.add(s+comp.charAt(i));
+				
+			
+			
+			return result;
 		}
 		
 	}
