f1b5e4b08110a8fdf6c183514bf7d2c530a63023
Yohan Park <pyh95510@gmail.com>
2022-10-05 03:21:43 -0400

    solution and comment

--- src/practicing/LetterCombinationsofaPhoneNumber17.java
+++ src/practicing/LetterCombinationsofaPhoneNumber17.java
@@ -12,7 +12,7 @@ public class LetterCombinationsofaPhoneNumber17 {
 			String[] comp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 			
 			
-			
+			answer.add("");
 			for(int i = 0 ; i<digits.length(); i++) {
 				answer = combination(comp[digits.charAt(i)-'0'],answer);
 			}
@@ -36,3 +36,12 @@ public class LetterCombinationsofaPhoneNumber17 {
 	}
 
 }
+/*I had so much fun to solve this problem. A problem mixed with string always gives a hard time but this one doesn't. Of course I got some help
+ * to solve this problem but it was so fun to solve this problem. The basic idea for this problem is this. First we create a string array that
+ * can store each mapped characters. Then create a method that could combine the input digits and all possible characters. The main idea here is
+ * that we first add string to answer"" like this. Unless it will becomes empty list in String s: answer.
+ * Basic idea for helper method is that every time we recursively call helper method, we add the result to the answer List<String>. Next time we
+ * get the digit input, we add all the possible input possible digits' information from String comp[] array and add it to the result List<String>
+ * By so, we are storing up the result until there is no result. The worst case of this method is 9999 because the problem's constraint says the digit
+ * will be 0~4. */
+ 
