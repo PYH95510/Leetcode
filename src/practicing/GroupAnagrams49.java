7a9663f871bdad47b28129e27594fd4bc7b7a8aa
Yohan Park <pyh95510@gmail.com>
2022-10-08 19:40:50 -0400

    comment for sorted solution

--- src/practicing/GroupAnagrams49.java
+++ src/practicing/GroupAnagrams49.java
@@ -33,5 +33,7 @@ public class GroupAnagrams49 {
 	
 }
 /*Using various values for the key and convert them is kind of tricky for this question. However, basic idea is  not that difficult for this question.
- * */
+ *First, getting string value from the input and then convert strings to array of characters. It is because we are trying to sort them. After we sort,
+ *we generate key value for the sorted string by using String.valueOf(). After that, if(!ans.containsKey(key), we instantiate ans.put(key,new ArrayList();
+ *and then get key and add (s) which is not a sorted string and which couldn't be sorted because we need not sorted string as a result.*/
  
