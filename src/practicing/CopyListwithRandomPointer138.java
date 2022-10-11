edd972c2ce332e8bd3ec328b7d996a8f03077dab
Yohan Park <pyh95510@gmail.com>
2022-10-10 22:08:53 -0400

    recursive solution

--- src/practicing/CopyListwithRandomPointer138.java
+++ src/practicing/CopyListwithRandomPointer138.java
@@ -1,9 +1,52 @@
 package practicing;
 
+import java.util.HashMap;
+
 public class CopyListwithRandomPointer138 {
 
-	public CopyListwithRandomPointer138() {
-		// TODO Auto-generated constructor stub
+	
+	
+	class Node {
+	    public int val;
+	    public Node next;
+	    public Node random;
+
+	    public Node() {}
+
+	    public Node(int _val,Node _next,Node _random) {
+	        val = _val;
+	        next = _next;
+	        random = _random;
+	    }
+	};
+	
+	public class Solution {
+	  
+	  HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+	  public Node copyRandomList(Node head) {
+
+	    if (head == null) {
+	      return null;
+	    }
+
+	    
+	    if (this.visitedHash.containsKey(head)) {
+	      return this.visitedHash.get(head);
+	    }
+
+	    
+	    Node node = new Node(head.val, null, null);
+
+	    
+	    this.visitedHash.put(head, node);
+
+	    
+	    node.next = copyRandomList(head.next);
+	    node.random = copyRandomList(head.random);
+
+	    return node;
+	  }
 	}
 
 }
