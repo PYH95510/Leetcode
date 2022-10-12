07c17eb6d158e79953eb0832136d5351e5eb88d8
Yohan Park <pyh95510@gmail.com>
2022-10-12 02:45:09 -0400

    solution that does not change input

---
+++ src/practicing/NumberofIsland200.java
@@ -0,0 +1,57 @@
+package practicing;
+
+public class NumberofIsland200 {
+
+	class Solution {
+	    
+	    public int numIslands(char[][] grid) {
+	        
+	        if (grid == null || grid.length == 0)return 0;
+	            
+	     
+	        int row = grid.length;
+	        int column = grid[0].length;
+	        boolean[][] marked = new boolean[row][column]; 
+	        int islandCount = 0;
+	    
+	        for (int i = 0; i < row; ++i) {
+	            for (int j = 0; j < column; ++j) {
+	                if (!marked[i][j] && grid[i][j] == '1') {
+	                    dfs(i, j, grid, marked);
+	                    ++islandCount;
+	                }
+	            }
+	        }
+	  
+	        return islandCount;
+
+	    }
+
+
+	    private void dfs(int i, int j, char[][] grid, boolean[][] marked) {
+	  
+	        int row = grid.length; 
+	        int column = grid[0].length;
+	         
+	        int x = i;
+	        int y = j;
+	        
+	            
+	        if(x>= 0 && x< row && y >= 0 && y < column){
+	            if (!marked[x][y] && grid[x][y] == '1'){
+	        marked[i][j] = true;
+	        dfs(x-1,y,grid,marked);
+	        dfs(x+1,y,grid,marked);
+	        dfs(x,y-1,grid,marked);
+	        dfs(x,y+1,grid,marked);
+	            }
+	        }
+	        
+	    
+
+	    }
+	    
+	    
+	}
+
+}
