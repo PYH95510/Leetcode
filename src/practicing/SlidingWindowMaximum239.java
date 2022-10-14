4c5e97fa41090977595109c3796cb2632b7e97ef
Yohan Park <pyh95510@gmail.com>
2022-10-13 23:32:53 -0400

    burte force answer

--- src/practicing/SlidingWindowMaximum239.java
+++ src/practicing/SlidingWindowMaximum239.java
@@ -3,10 +3,24 @@ package practicing;
 public class SlidingWindowMaximum239 {
 
 	class Solution {
-	    public int[] maxSlidingWindow(int[] nums, int k) {
-	        int n = nums.length;
-	        i
-	    }
+		public int[] maxSlidingWindow(int[] nums, int k) {
+			int n = nums.length;
+			if(n*k == 0) return new int[0];
+			int[] answers = new int[n-k+1];
+			
+			for(int i = 0; i < n-k+1; i++) {
+				int max = Integer.MIN_VALUE;
+				for(int j = i; j <i+k;j++) {
+					max = Math.max(max, nums[j]);
+				}
+				answers[i] = max;
+			}
+			
+			
+			return answers;
+		}
+	        
+	    
 	}
 
 }
