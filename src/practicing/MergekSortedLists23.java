b0a7734f9ec6b61f69c1979d3ea4ce972aa58d94
Yohan Park <pyh95510@gmail.com>
2022-10-18 01:39:52 -0400

    new inline comment to help fully understand

--- src/practicing/MergekSortedLists23.java
+++ src/practicing/MergekSortedLists23.java
@@ -18,22 +18,26 @@ public class MergekSortedLists23 {
 	  }
 	 
 	
-	    public static ListNode mergeKLists(ListNode[] lists) {
-	        Queue<ListNode> heap = new PriorityQueue <ListNode> ((l1,l2)->l1.val-l2.val); //instantiate priority queue using lists and 
-	        ListNode head = new ListNode(0), tail = head;
-	        
-	        for (ListNode node : lists){
-	            if (node != null) heap.offer(node);
-	        }
-	        
-	        while (!heap.isEmpty()) {
-	            tail.next = heap.poll();
-	            tail = tail.next;
-	            if (tail.next != null) heap.offer(tail.next);
-	        }
-	        
-	        return head.next;
-	    }
+	  
+		    public ListNode mergeKLists(ListNode[] lists) {
+		        Queue<ListNode> heap = new PriorityQueue <ListNode> ((l1,l2)->l1.val-l2.val); //instantiate priority queue using lists and 
+		        ListNode head = new ListNode(0), tail = head;
+		        
+		        for (ListNode node : lists){
+		            if (node != null) heap.offer(node);
+		        }
+		        
+		        while (!heap.isEmpty()) {
+		            tail.next = heap.poll(); // this will automatically add to head's next as well it is because we first initialize tail = head as well. It is refering the object's address. so if we are changing tail.next's address, then head.next's address will be changed as well. We are adding the result to the tail.next which is head.next. Also, even if we have  already changed tail, we have set head's next to list 1 which is object as well, head's next is referring to it. Therefore, if we are appending to 1's next, it will appending to head's next as well.
+		            tail = tail.next; 
+		            if (tail.next != null) heap.offer(tail.next);
+		            // it is referring new tail's next because we just changed tail to tail.next not the tail.next that we assigned above
+		        }
+		        
+		        return head.next;
+		    }
+		    
+		
 	    
 	
 	    public static void main(String[] args) {
