ced8083b3b15acebebad3706ba8f2e5463014a20
Yohan Park <pyh95510@gmail.com>
2022-10-24 02:05:37 -0400

    k closest points to origin 973

---
+++ src/practicing/KClosestPointstoOrigin973.java
@@ -0,0 +1,16 @@
+package practicing;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KClosestPointstoOrigin973 {
+
+	class solution{
+		public int[][] kClosest(int[][] points, int k) {
+			Queue<int[]>priority = new PriorityQueue<int[]>(k,new comparator(<int[]>()) {
+				
+			})
+		}
+	}
+
+}
