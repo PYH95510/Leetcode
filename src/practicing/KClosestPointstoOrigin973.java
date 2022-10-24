773b8803c4d166d3047282a23d2201d67a4a4e8a
Yohan Park <pyh95510@gmail.com>
2022-10-24 02:18:09 -0400

    solution

--- src/practicing/KClosestPointstoOrigin973.java
+++ src/practicing/KClosestPointstoOrigin973.java
@@ -1,5 +1,6 @@
 package practicing;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -7,9 +8,22 @@ public class KClosestPointstoOrigin973 {
 
 	class solution{
 		public int[][] kClosest(int[][] points, int k) {
-			Queue<int[]>priority = new PriorityQueue<int[]>(k,new comparator(<int[]>()) {
-				
-			})
+			 if(k == points.length) return points;
+			Queue<int[]> priority = new PriorityQueue<int[]>(k,new Comparator<int[]>() { //k is the initial comparator size
+				public int compare(int[] a, int[] b) {
+	                return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
+	            }
+	        });
+			
+			for(int[] point:points) { //from for each, int[] point will be iterated as array set.
+				priority.add(point);
+				if(priority.size()>k)
+					priority.poll();
+			}
+			
+			
+			return priority.toArray(new int[0][0]);
+			
 		}
 	}
 
