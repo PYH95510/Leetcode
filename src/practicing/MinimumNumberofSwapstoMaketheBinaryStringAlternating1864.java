fbf3ca4e6ceff8d79620ed94a4de9bc68727a9ed
Yohan Park <pyh95510@gmail.com>
2022-11-01 03:56:00 -0400

    solution with comment

--- src/practicing/MinimumNumberofSwapstoMaketheBinaryStringAlternating1864.java
+++ src/practicing/MinimumNumberofSwapstoMaketheBinaryStringAlternating1864.java
@@ -14,15 +14,15 @@ public class MinimumNumberofSwapstoMaketheBinaryStringAlternating1864 {
 	           
 	        // for odd number, if 1 is more than 0, then it is inevitably placed in even position, the other way is same 
 	        if(one > zero)
-	            return checkSwaps(s.toCharArray(),'1');
+	            return replace(s.toCharArray(),'1');
 	        else if(zero > one)
-	            return checkSwaps(s.toCharArray(),'0');
+	            return replace(s.toCharArray(),'0');
 	        //else for odd number, just swap for both and then get the min number from either one.
 	        else
-	            return Math.min(checkSwaps(s.toCharArray(),'1'),checkSwaps(s.toCharArray(),'0'));
+	            return Math.min(replace(s.toCharArray(),'1'),replace(s.toCharArray(),'0'));
 	    }
 	    
-	    public int checkSwaps(char[] s, char symbol){
+	    public int replace(char[] s, char symbol){
 	        int swaps=0;
 	        for(int i=0;i<s.length;i++){
 	            if(s[i] == symbol && i%2 != 0) // we are using modulo to avoid situation where after we change digit, then the adjacent digit will be not the same number.
@@ -34,3 +34,8 @@ public class MinimumNumberofSwapstoMaketheBinaryStringAlternating1864 {
 	//time complexity for both is o(n) and space complexity is o(1)
 
 }
+//11100 10101 00111 10101
+/*This is more likely to be math problem. First we should come up with the idea that if there are more I would say one biased digit, then it is
+ * impossible to make alternating. If it is not, we can then make it. After that, we then, check if there is biased digit is on the even position
+ * it is because if it is on the even position, it is inevitably meet with the same number so we just need to check how many are they.*/
+ 
