39a0c4875389c917d2e38f868407df0a2304a50a
Yohan Park <pyh95510@gmail.com>
2022-11-01 22:31:11 -0400

    swith case solution

--- src/practicing/RomantoInteger13.java
+++ src/practicing/RomantoInteger13.java
@@ -2,8 +2,43 @@ package practicing;
 
 public class RomantoInteger13 {
 
-	public RomantoInteger13() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public int romanToInt(String s) {
+	        int answer = 0, tmtnumber = 0;
+	        for(int i = s.length()-1; i >= 0; i--){
+	            switch(s.charAt(i)){
+	                case 'I' : 
+	                    tmtnumber = 1;
+	                    break;
+	                case 'V' :
+	                    tmtnumber = 5;
+	                    break;
+	                case 'X' :
+	                    tmtnumber = 10;
+	                    break;
+	                case 'L' :
+	                    tmtnumber = 50;
+	                    break;
+	                case 'C' :
+	                    tmtnumber = 100;
+	                    break;
+	                case 'D' :
+	                    tmtnumber = 500;
+	                    break;
+	                case 'M' :
+	                    tmtnumber = 1000;
+	                    break;
+	            }
+	            if(4*tmtnumber<answer)answer -= tmtnumber;
+	                else answer+= tmtnumber;
+	                //we should multiply 4 because answer could be up to 3999
+	                //in the answer, previous number is in. tmtnumber is new number, according to roman number, new input number should always bigger than the previous number. However, even though we multiplied 4 times, it is still smaller than the previous number, then it means IXC comes before for either VLD or XCM to make it smaller.
+	            
+	        }
+	        return answer;
+	        
+	        
+	    }
 	}
 
 }
