466cd89265ae6f0d5b017c6f7f9ede96aa12f886
Yohan Park <pyh95510@gmail.com>
2022-11-02 01:30:42 -0400

    o(n) substring solution

--- src/practicing/LongestCommonPrefix14.java
+++ src/practicing/LongestCommonPrefix14.java
@@ -2,8 +2,22 @@ package practicing;
 
 public class LongestCommonPrefix14 {
 
-	public LongestCommonPrefix14() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public String longestCommonPrefix(String[] strs) {
+	        String prefix = strs[0];
+	        
+	        
+	        for(int i = 1; i<strs.length; i++){
+	            while(strs[i].indexOf(prefix)!=0){
+	                prefix = prefix.substring(0,prefix.length()-1);
+	                if(prefix.length() == 0)return"";
+	            }
+	        }
+	        
+	        return prefix;
+	    }
+	    
+	    
 	}
 
 }
