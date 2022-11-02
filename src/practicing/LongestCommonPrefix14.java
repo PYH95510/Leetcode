7947393586e882ab215777142a7a8e6142db38c2
Yohan Park <pyh95510@gmail.com>
2022-11-02 01:42:45 -0400

    solution and comment

--- src/practicing/LongestCommonPrefix14.java
+++ src/practicing/LongestCommonPrefix14.java
@@ -21,3 +21,28 @@ public class LongestCommonPrefix14 {
 	}
 
 }
+//class Solution {
+//    public String longestCommonPrefix(String[] strs) {
+//        if (strs == null || strs.length == 0)
+//            return "";
+//        Arrays.sort(strs);
+//        String first = strs[0];
+//        String last = strs[strs.length - 1];
+//        
+//        int c = 0;
+//        while(c<first.length()){
+//       if (first.charAt(c) == last.charAt(c))
+//                c++;
+//            else
+//                break;
+//        }
+//        return c == 0 ? "" : first.substring(0, c);
+//        
+//        
+//    }
+//}
+/*This is LC easy section. However, it is not easy to solve and we have tons of things to learn here. The uncommented solution is O(n) timecomplexity
+ * and O(1) space complexity solution. Below is o(nlogn) timecomplexity solution. However, below solution is very intuitive and easy to understand.
+ * Above solution is very fast and easy to understand. We first set first element array as prefix and then bring it into for loop. By using while
+ * loop and the indexOf, we iterate and eliminate one characters at a time using substring starting at the end. Because indexOf(string) treat string
+ * as a whole chunk of index element, we could use this method. If we cannot find any comment element, it will eliminate all the characters.*/
