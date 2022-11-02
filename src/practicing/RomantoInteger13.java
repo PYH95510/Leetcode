bd125c3bffc97e559fab14068197c26b50e3c7e7
Yohan Park <pyh95510@gmail.com>
2022-11-01 23:05:27 -0400

    hashmap solution

--- src/practicing/RomantoInteger13.java
+++ src/practicing/RomantoInteger13.java
@@ -37,7 +37,29 @@ public class RomantoInteger13 {
 	        }
 	        return answer;
 	        
-	        
+//	        class Solution {
+//	            public int romanToInt(String s) {
+//	                Map <Character , Integer> romans = new HashMap <>();
+//	                romans.put('I',1);
+//	                romans.put('V',5);
+//	                romans.put('X',10);
+//	                romans.put('L',50);
+//	                romans.put('C',100);
+//	                romans.put('D',500);
+//	                romans.put('M',1000);
+//	                
+//	                int answer = 0;
+//	                
+//	                
+//	                for(int i = 0 ; i<s.length()-1; i++){
+//	                   if(romans.get(s.charAt(i)) < romans.get(s.charAt(i+1))) answer -= romans.get(s.charAt(i));
+//	                    else answer += romans.get(s.charAt(i));
+//	                }
+//	                
+//	                return answer+romans.get(s.charAt(s.length()-1));
+//	            }
+//	            
+//	        }
 	    }
 	}
 
