d6489db146e098642443a4099a3a0d724ed1fd6c
Yohan Park <pyh95510@gmail.com>
2022-11-01 22:32:37 -0400

    minor change

--- src/practicing/RomantoInteger13.java
+++ src/practicing/RomantoInteger13.java
@@ -31,7 +31,7 @@ public class RomantoInteger13 {
 	            }
 	            if(4*tmtnumber<answer)answer -= tmtnumber;
 	                else answer+= tmtnumber;
-	                //we should multiply 4 because answer could be up to 3999
+	                //we should multiply 4 because answer could be up to 80 or 800
 	                //in the answer, previous number is in. tmtnumber is new number, according to roman number, new input number should always bigger than the previous number. However, even though we multiplied 4 times, it is still smaller than the previous number, then it means IXC comes before for either VLD or XCM to make it smaller.
 	            
 	        }
