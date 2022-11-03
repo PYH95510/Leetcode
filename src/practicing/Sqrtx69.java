72a4b9019813cbc1ef9c3839ad09798a4c21bdc0
Yohan Park <pyh95510@gmail.com>
2022-11-03 02:48:29 -0400

    solution with comment

--- src/practicing/Sqrtx69.java
+++ src/practicing/Sqrtx69.java
@@ -10,7 +10,7 @@ public class Sqrtx69 {
 	        
 	        while(start <= end) {
 	        	mid = start+(end-start)/2;
-	        	num = (long)mid*mid;
+	        	num = (long)mid*mid; //this is needed to find square root without using built in library.
 	        	if(num>x)end = mid-1;
 	        	else if(num < x) { start = mid +1;
 	        		
@@ -24,3 +24,7 @@ public class Sqrtx69 {
 	}
 
 }
+/*This problem is based on binary search. I was pretty satisfied because I came up with the solution by myself. By iterating solution writing on the
+ * paper. You can easily find  out what should be done. As I commented in the line, you would easily know what end = x/2 is supposed to be meaning.
+ * once it is clear, it is very easy because rest of it is pretty much same as binary search. time complexity is O(log n) and space complexity is O(1).*/
+ 
