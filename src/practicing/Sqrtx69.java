65aa9f06e0b0a6beabbed7a614d860e4fe468e6e
Yohan Park <pyh95510@gmail.com>
2022-11-03 02:45:34 -0400

    solution

--- src/practicing/Sqrtx69.java
+++ src/practicing/Sqrtx69.java
@@ -2,8 +2,25 @@ package practicing;
 
 public class Sqrtx69 {
 
-	public Sqrtx69() {
-		// TODO Auto-generated constructor stub
+	class Solution{
+		public int mySqrt(int x) {
+	        if(x<2)return x;
+	        long num;
+	        int mid, start = 2 , end = x/2; //this is the trick to set the end. The square root is always smaller than the x/2.
+	        
+	        while(start <= end) {
+	        	mid = start+(end-start)/2;
+	        	num = (long)mid*mid;
+	        	if(num>x)end = mid-1;
+	        	else if(num < x) { start = mid +1;
+	        		
+	        	}else {
+	        		return mid;
+	        	}
+	        }
+	        
+	        return end;		
+	    }
 	}
 
 }
