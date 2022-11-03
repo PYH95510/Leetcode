511edef5b1d2c81214f7539c83e146b7035fae05
Yohan Park <pyh95510@gmail.com>
2022-11-02 23:15:56 -0400

    not optimized solution

--- src/practicing/RemoveDuplicatesFromSortedArray26.java
+++ src/practicing/RemoveDuplicatesFromSortedArray26.java
@@ -6,20 +6,26 @@ import java.util.Set;
 
 public class RemoveDuplicatesFromSortedArray26 {
 
-	  public int removeDuplicates(int[] nums) {
-	        Set <Integer> duplicate = new HashSet<Integer>();
-	        
-	        for(int i = 0 ; i < nums.length ; i++){
-	            if(duplicate.contains(nums[i])){
-	                nums[i]=101;
-	                
-	            }else{
-	                duplicate.add(nums[i]);
-	            }
-	        }
-	        
-	        Arrays.sort(nums);
-	        return duplicate.size();
-	    }
+//	  public int removeDuplicates(int[] nums) {
+//	        Set <Integer> duplicate = new HashSet<Integer>();
+//	        
+//	        for(int i = 0 ; i < nums.length ; i++){
+//	            if(duplicate.contains(nums[i])){
+//	                nums[i]=101;
+//	                
+//	            }else{
+//	                duplicate.add(nums[i]);
+//	            }
+//	        }
+//	        
+//	        Arrays.sort(nums);
+//	        return duplicate.size();
+//	    }
 
+	  class Solution{
+		  public int removeDuplicates(int[] nums) {
+			  
+		  }
+	  }
+	  
 }
