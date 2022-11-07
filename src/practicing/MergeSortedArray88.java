cc1e7b0321a24af979cd3fe1e010aaffbc5f4e70
yohan <pyh95510@gmail.com>
2022-11-06 23:48:34 -0500

    push

--- src/practicing/MergeSortedArray88.java
+++ src/practicing/MergeSortedArray88.java
@@ -5,29 +5,53 @@ import java.util.Deque;
 
 public class MergeSortedArray88 {
 
+//	class Solution {
+//	    public void merge(int[] nums1, int m, int[] nums2, int n) {
+//	        int[] answer = new int[nums1.length];
+//	        Deque <Integer> first = new ArrayDeque <Integer>();
+//	        Deque <Integer> second = new ArrayDeque <Integer>();
+//	        
+//	        for(int i = 0 ; i<n; i++){
+//	            first.offer(nums1[i]);
+//	            second.offer(nums2[i]);
+//	        }
+//	        
+//	        for(int i = 0 ; i<nums1.length; i++){
+//	            
+//	            if(!first.isEmpty() && first.peek() <= second.peek()){
+//	                answer[i] = first.poll();
+//	            }else{
+//	                answer[i] = second.poll();
+//	            }
+//	        }
+//	        nums1 = answer;
+//	        
+//	    }
+//	    
+//	    
+//	}
 	class Solution {
 	    public void merge(int[] nums1, int m, int[] nums2, int n) {
-	        int[] answer = new int[nums1.length];
-	        Deque <Integer> first = new ArrayDeque <Integer>();
-	        Deque <Integer> second = new ArrayDeque <Integer>();
+	        int index1 = m-1;
+	        int index2 = m-1;
 	        
-	        for(int i = 0 ; i<n; i++){
-	            first.offer(nums1[i]);
-	            second.offer(nums2[i]);
-	        }
 	        
-	        for(int i = 0 ; i<nums1.length; i++){
+	        
+	        //two pointer + one pointer = three pointer
+	        for(int i = nums1.length-1 ; 0<=i; i--){
+	            if(index2<0)break;
 	            
-	            if(!first.isEmpty() && first.peek() <= second.peek()){
-	                answer[i] = first.poll();
+	            if(index1 >= 0 && nums1[index1] > nums2[index2]){
+	                nums1[i] =nums1[index1];
+	                index1--;
 	            }else{
-	                answer[i] = second.poll();
+	                nums1[i]=nums2[index2];
+	                index2--;
 	            }
+	            
 	        }
-	        nums1 = answer;
 	        
 	    }
-	    
 	    
 	}
 
