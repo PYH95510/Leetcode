bd81653ee6d04a061a715c400fa67775d6ba6c58
Yohan <pyh95510@gmail.com>
2022-11-13 02:40:34 -0500

    two string reverse solution

--- src/practicing/ValidPalindrome125.java
+++ src/practicing/ValidPalindrome125.java
@@ -17,4 +17,33 @@ public class ValidPalindrome125 {
 	        return true;
 	    }
 	}
+//	class Solution {
+//		  public boolean isPalindrome(String s) {
+//		    StringBuilder builder = new StringBuilder();
+//
+//		    for (char ch : s.toCharArray()) {
+//		      if (Character.isLetterOrDigit(ch)) {
+//		        builder.append(Character.toLowerCase(ch));
+//		      }
+//		    }
+//
+//		    String filteredString = builder.toString();
+//		    String reversedString = builder.reverse().toString();
+//
+//		    return filteredString.equals(reversedString);
+//		  }
+//
+//		  /** An alternate solution using Java 8 Streams */
+//		  public boolean isPalindromeUsingStreams(String s) {
+//		    StringBuilder builder = new StringBuilder();
+//
+//		    s.chars()
+//		        .filter(c -> Character.isLetterOrDigit(c))
+//		        .mapToObj(c -> Character.toLowerCase((char) c))
+//		        .forEach(builder::append);
+//
+//		    return builder.toString().equals(builder.reverse().toString());
+//		  }
+//		}
 }
+/*When it comes to palindrme question, two things should always we have to have in minde. Either we will compare */
