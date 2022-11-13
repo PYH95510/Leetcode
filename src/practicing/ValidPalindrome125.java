e2e646b1fd32859ca9cfb3f72e9389a9f07127cc
Yohan <pyh95510@gmail.com>
2022-11-13 02:37:05 -0500

    solution

--- src/practicing/ValidPalindrome125.java
+++ src/practicing/ValidPalindrome125.java
@@ -2,8 +2,19 @@ package practicing;
 
 public class ValidPalindrome125 {
 
-	public ValidPalindrome125() {
-		// TODO Auto-generated constructor stub
+	class Solution {
+	    public boolean isPalindrome(String s) {
+	        for (int i = 0, j = s. length() - 1 ; i < j ; i++, j--){
+	            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+	                i++;
+	            }
+	            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+	                j--;
+	            }
+	            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+	            return false;
+	        }
+	        return true;
+	    }
 	}
-
 }
