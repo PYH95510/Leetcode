da94e8573756ec3a46fd014dbc9e9c0cf04040a8
Yohan <pyh95510@gmail.com>
2022-11-14 06:09:39 -0500

    o(n) o(1) fast,slow solution

--- src/practicing/LinkedListCycle141.java
+++ src/practicing/LinkedListCycle141.java
@@ -14,24 +14,38 @@ public class LinkedListCycle141 {
 		      }
 		  }
 	
+//	public class Solution {
+//		public boolean hasCycle(ListNode head) {
+//		       Set<ListNode>cycle = new HashSet<ListNode>();
+//		        
+//		        while(head!=null){
+//		            if(cycle.contains(head))return true;
+//		            
+//		            cycle.add(head);
+//		            head = head.next;
+//		        }
+//		        
+//		        return false;
+//		        
+//		    }
+//	}
 	public class Solution {
-		public boolean hasCycle(ListNode head) {
-		       Set<ListNode>cycle = new HashSet<ListNode>();
-		        
-		        while(head!=null){
-		            if(cycle.contains(head))return true;
-		            
-		            cycle.add(head);
-		            head = head.next;
-		        }
-		        
-		        return false;
-		        
-		    }
+	    public boolean hasCycle(ListNode head) {
+	        if (head == null) return false;
+	        
+	        ListNode slow = head;
+	        ListNode fast = head.next;
+	        while (slow != fast) {
+	            if (fast == null || fast.next == null) return false;
+	            slow = slow.next;
+	            fast = fast.next.next;
+	        }
+	        
+	        return true;
+	    }
 	}
-
 	
 }
 /*using hash algorithm has o(n) time complexity (using while loop) (add and contains for hash time complexity is only o(1). Space complexity for Hash
- * is O(n)*/
+ * is O(n). However, I prefer the below solution.*/
  
