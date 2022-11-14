c9172fd0151407916da3aa9112f1091976cdfe46
Yohan <pyh95510@gmail.com>
2022-11-14 05:03:09 -0500

    Linked List Cycle 141

---
+++ src/practicing/LinkedListCycle141.java
@@ -0,0 +1,20 @@
+package practicing;
+
+public class LinkedListCycle141 {
+	
+	class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) {
+		          val = x;
+		          next = null;
+		      }
+		  }
+	
+	public class Solution {
+		public boolean hasCycle(ListNode head) {
+
+		}
+	}
+
+}
