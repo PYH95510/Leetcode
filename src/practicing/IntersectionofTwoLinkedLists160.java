944e661c5eafff9bdfa118097abca857cbb90a7a
Yohan <pyh95510@gmail.com>
2022-11-17 03:42:25 -0500

    hash map solution O(m+n) time complexity o(n) space complexity

--- src/practicing/IntersectionofTwoLinkedLists160.java
+++ src/practicing/IntersectionofTwoLinkedLists160.java
@@ -2,8 +2,26 @@ package practicing;
 
 public class IntersectionofTwoLinkedLists160 {
 
-	public IntersectionofTwoLinkedLists160() {
-		// TODO Auto-generated constructor stub
-	}
+//	public class Solution {
+//	    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//	        Set<ListNode> nodesInB = new HashSet<ListNode>();
+//
+//	        while (headB != null) {
+//	            nodesInB.add(headB);
+//	            headB = headB.next;
+//	        }
+//
+//	        while (headA != null) {
+//	            // if we find the node pointed to by headA,
+//	            // in our set containing nodes of B, then return the node
+//	            if (nodesInB.contains(headA)) {
+//	                return headA;
+//	            }
+//	            headA = headA.next;
+//	        }
+//
+//	        return null;
+//	    }
+//	}
 
 }
