cddbd9703b5c8009761428ab032b0758909d1688
Yohan <pyh95510@gmail.com>
2022-11-26 07:27:34 -0500

    solution O(nlogn) o(1)

--- src/practicing/MajorityElement169.java
+++ src/practicing/MajorityElement169.java
@@ -1,5 +1,11 @@
 package practicing;
 
-public class MajorityElement169 {
+import java.util.Arrays;
 
+public class MajorityElement169 {
+	
+	public int majorityElement(int[] nums) {
+		Arrays.sort(nums);
+		return nums[nums.length/2];
+	}
 }
