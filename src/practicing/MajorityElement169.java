9e5bae666c38c0befe1603a2f7dc1fce7277bd8c
Yohan <pyh95510@gmail.com>
2022-11-26 07:48:16 -0500

    o(n) time complexity o(1) space complexity solution

--- src/practicing/MajorityElement169.java
+++ src/practicing/MajorityElement169.java
@@ -5,7 +5,16 @@ import java.util.Arrays;
 public class MajorityElement169 {
 	
 	public int majorityElement(int[] nums) {
-		Arrays.sort(nums);
-		return nums[nums.length/2];
+//		Arrays.sort(nums);
+//		return nums[nums.length/2];
+		Integer candidate = null;
+		int count = 0;
+		
+		for(int num : nums) {
+			if(count == 0)candidate = num;
+			
+			count +=(candidate == num)? 1 : -1;
+		}
+		return candidate;
 	}
 }
