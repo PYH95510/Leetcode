4c390382cd9fb31bd10f0358026ecd7d6b5ddf45
Yohan <pyh95510@gmail.com>
2022-12-08 21:15:38 -0500

    Excel Sheet Column Number

--- src/practicing/ExcelSheetColumnNumber171.java
+++ src/practicing/ExcelSheetColumnNumber171.java
@@ -1,13 +1,30 @@
 package practicing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExcelSheetColumnNumber171 {
 	
-	class Solution{
+	
 		
-		public int titleToNumber(String columnTitle) {
-			Map<Character, Integer> alphabet = new HashMap <Character, Integer>();
-			
+		class Solution {
+		    public int titleToNumber(String s) {
+		        int result = 0;
+
+		        Map < Character, Integer > alpha_map = new HashMap < > ();
+		        for (int i = 0; i < 26; i++) {
+		            int c = i + 65; // Decimal 65 in ASCII corresponds to char 'A'
+		            alpha_map.put((char) c, i + 1);
+		        }
+
+		        int n = s.length();
+		        for (int i = 0; i < n; i++) {
+		            char cur_char = s.charAt(n - 1 - i);
+		            result += (alpha_map.get(cur_char) * (Math.pow(26, i)));
+		        }
+		        return result;
+		    }
 		}
-	}
+	
 
 }
