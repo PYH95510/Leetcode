cfa18de83d5441d29ccb8f8873c8745a21aaab9e
Yohan <pyh95510@gmail.com>
2022-12-09 02:02:12 -0500

    solution for reverse bit

--- src/practicing/ReverseBits190.java
+++ src/practicing/ReverseBits190.java
@@ -4,8 +4,15 @@ public class ReverseBits190 {
 	
 	public class soluition{
 		public int reverseBits(int n) {
-			
-			return n;
+			n = n>>>16 | n << 16; //2byte swap
+		    n = (n&0xff00ff00)>>>8 | (n&0x00ff00ff) <<8; //1byte swap
+		    n = (n&0xf0f0f0f0)>>>4 | (n&0x0f0f0f0f) <<4; //4bit swap
+		    n = (n&0xcccccccc)>>>2 | (n&0x33333333) <<2;//2bit swap
+		    n = (n&0xaaaaaaaa)>>>1 | (n&0x55555555) <<1;//1bit swap
+		    
+		    return n;
+		    
+		
 		}
 	}
 }
