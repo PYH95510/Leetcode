c9885eb861a0747b463cd03a353a394b85127abd
Yohan <pyh95510@gmail.com>
2022-12-09 02:03:00 -0500

    inline comment

--- src/practicing/ReverseBits190.java
+++ src/practicing/ReverseBits190.java
@@ -9,6 +9,9 @@ public class ReverseBits190 {
 		    n = (n&0xf0f0f0f0)>>>4 | (n&0x0f0f0f0f) <<4; //4bit swap
 		    n = (n&0xcccccccc)>>>2 | (n&0x33333333) <<2;//2bit swap
 		    n = (n&0xaaaaaaaa)>>>1 | (n&0x55555555) <<1;//1bit swap
+			
+			// I am not familiar with the bitwise operation. | is or when it comes to bitwise operation.
+			
 		    
 		    return n;
 		    
