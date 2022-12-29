d8bff664a93143e4b6bdabd4eadef64ea6c51e06
Yohan <pyh95510@gmail.com>
2022-12-29 01:59:38 -0500

    solution

--- src/practicing/ReverseLinkedList206.java
+++ src/practicing/ReverseLinkedList206.java
@@ -1,5 +1,32 @@
 package practicing;
 
-public class ReverseLinkedList206 {
 
+public class ReverseLinkedList206 {
+	
+	public class ListNode{
+		int val;
+		ListNode next;
+		ListNode(){
+			
+		}
+		
+		ListNode(int val){
+			this.val = val;
+		}
+		
+		ListNode(int val, ListNode next) {
+			this.val = val; 
+			this.next = next; 
+			}
+		
+	}
+	
+	class Solution{
+		
+		public ListNode reverseList(ListNode head) {
+			
+		}
+		
+	}
+	
 }
