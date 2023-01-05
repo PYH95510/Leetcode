369603e5068eed3f556dad2ab31c82e5e4a09353
Yohan <pyh95510@gmail.com>
2023-01-04 23:16:27 -0500

    solution using bit manipulation

--- src/practicing/MissingNumber268.java
+++ src/practicing/MissingNumber268.java
@@ -1,5 +1,13 @@
 package practicing;
 
 public class MissingNumber268 {
-
+	class Solution {
+	    public int missingNumber(int[] nums) {
+	        int missing = nums.length;
+	        for (int i = 0; i < nums.length; i++) {
+	            missing ^= i ^ nums[i];
+	        }
+	        return missing;
+	    }
+	}
 }
