a6a499a22cc3c74f1d32dc0ead9a4917bf53144a
Yohan <pyh95510@gmail.com>
2023-01-12 00:46:22 -0500

    solution

--- src/practicing/MoveZeroes283.java
+++ src/practicing/MoveZeroes283.java
@@ -1,7 +1,23 @@
 package practicing;
 
 public class MoveZeroes283 {
-	public void moveZeroes(int[] nums) {
+	class Solution{
+		public void moveZeroes(int[] nums) {
+			int nonzeroindex =0;
+			
+			for(int i = 0 ; i<nums.length; i++) {
+				if(i != 0) {
+					nums[nonzeroindex] = nums[i];
+					nonzeroindex++;
+				}
+				
+			}
+			while(nonzeroindex<nums.length) {
+				nums[nonzeroindex++] = 0;
+			}
+			
+		}
 		
 	}
+	
 }
