45ad9a97bd66217c3f170a7b540f4d1683560450
Yohan <pyh95510@gmail.com>
2023-01-12 00:51:47 -0500

    solution comment

--- src/practicing/MoveZeroes283.java
+++ src/practicing/MoveZeroes283.java
@@ -21,3 +21,11 @@ public class MoveZeroes283 {
 	}
 	
 }
+
+
+/*This is another typical two pointer problem. I could come up with by myself. First we need nonzeroindex pointer. While we are iterating, whenever
+ * we face non zero factor, we add it to nums[nonzeroindex] array. Then, we increment nonzeroindex. After we move all the nonzero indexes to the
+ * front, left parts should be filled with zero. Therefore, we iterate with while loop until the nonzeroindex is less than nums.length.
+ * We do not need to worry about what if the numbers inside the array overlaps before they are moved forward? One thing that we need to remember
+ * is that in the worst case, the index of nonzeroindex and the length of i are equal.*/
+
